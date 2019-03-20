4250bf9b8e695eeebade1ad1336d825001c2aa68
MengyuHAN <mh3881@columbia.edu>
2019-03-20 16:32:55 -0400

    update

--- pages/image_upload/image.js
+++ pages/image_upload/image.js
@@ -1,12 +1,17 @@
 function readURL(input) {
     if (input.files && input.files[0]) {
-        var reader = new FileReader();
+        const reader = new FileReader()
 
         reader.onload = function (e) {
             $('#blah')
-                .attr('src', e.target.result);
-        };
+                .attr('src', e.target.result)
+        }
 
-        reader.readAsDataURL(input.files[0]);
+        reader.readAsDataURL(input.files[0])
     }
 }
+
+document.getElementById('submitImage').addEventListener('click', function() {
+    window.location.href = 'pages/music_play/music_list.html';
+});
+
